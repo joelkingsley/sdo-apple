87d5b37d2be7923b7bea6eb21915a35f54c0f037
Joel Kingsley <joelkingsley.r@gmail.com>
2022-04-08 23:27:38 +0530

    Initial Commit

---
+++ Shared/SDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDOApp.swift
+//  Shared
+//
+//  Created by Joel Kingsley on 08/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
