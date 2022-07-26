cc3d46b8d226238c79ea30089055541c301f2ef9
Joel Kingsley <joelkingsley.r@gmail.com>
2022-07-26 10:20:47 +0530

    Added commits

--- Shared/Services/Auth/FirebaseAuthService.swift
+++ Shared/Services/Auth/FirebaseAuthService.swift
@@ -14,6 +14,7 @@ class FirebaseAuthService: SDOAuthService {
       return GoogleSignInAuthenticator()
     }
     
+    /// Restores current user session from keychain if valid
     func restorePreviousSignIn() async -> AuthState {
         return await withCheckedContinuation { continuation in
             if let currentUser = Auth.auth().currentUser {
@@ -26,6 +27,7 @@ class FirebaseAuthService: SDOAuthService {
         }
     }
     
+    /// Signs in user with the google sign-in (pop-up) and then signs in on firebase
     func signInWithGoogle() async -> AuthState {
         do {
             let user: GIDGoogleUser = try await googleSignInAuthenticator.signIn()
@@ -37,20 +39,27 @@ class FirebaseAuthService: SDOAuthService {
                 withIDToken: idToken,
                 accessToken: authentication.accessToken
             )
-            do {
-                let authDataResult = try await Auth.auth().signIn(with: credential)
-                let user = authDataResult.user
-                return .signedIn(user)
-            } catch {
-                AppLogger.error("Unexpectedly got error while signing in with firebase: \(error)")
-                return .signedOut
-            }
+            return await signInOnFirebase(with: credential)
         } catch {
             AppLogger.error("Unexpectedly got error while signing in with google: \(error)")
             return .signedOut
         }
     }
     
+    /// Signs in to firebase using the given auth credentials
+    /// - note: To be invoked after authenticating with username-password or social sign-in
+    private func signInOnFirebase(with credential: AuthCredential) async -> AuthState {
+        do {
+            let authDataResult = try await Auth.auth().signIn(with: credential)
+            let user = authDataResult.user
+            return .signedIn(user)
+        } catch {
+            AppLogger.error("Unexpectedly got error while signing in with firebase: \(error)")
+            return .signedOut
+        }
+    }
+    
+    /// Signs in user out of all authenticators and firebase
     func signOut() -> Bool {
         do {
             googleSignInAuthenticator.signOut()
--- Shared/Services/Auth/SDOAuthService.swift
+++ Shared/Services/Auth/SDOAuthService.swift
@@ -8,7 +8,13 @@
 import Foundation
 
 protocol SDOAuthService {
+    /// Restores current user session from keychain if valid
     func restorePreviousSignIn() async -> AuthState
+
+    /// Signs in user with the google sign-in (pop-up) and then signs in on firebase
     func signInWithGoogle() async -> AuthState
+
+    /// Signs in to firebase using the given auth credentials
+    /// - note: To be invoked after authenticating with username-password or social sign-in
     func signOut() -> Bool
 }
