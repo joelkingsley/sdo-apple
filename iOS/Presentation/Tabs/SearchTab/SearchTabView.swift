bc066a86076a6982b1f64bc3890905661d4cfbc7
Joel Kingsley <joelkingsley.r@gmail.com>
2022-07-29 01:43:31 +0530

    Replaced MusicTab with SearchTab

---
+++ iOS/Presentation/Tabs/SearchTab/SearchTabView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchTabView.swift
+//  SDO (iOS)
+//
+//  Created by Joel Kingsley on 28/07/2022.
+//
+
+import SwiftUI
+
+struct SearchTabView: View {
+    var body: some View {
+        Text("Search Tab")
+    }
+}
+
+struct SearchTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchTabView()
+    }
+}
