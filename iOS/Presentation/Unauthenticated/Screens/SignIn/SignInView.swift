94c3f86530adad1024bd5ecd5d9b8575f159b3eb
Joel Kingsley <joelkingsley.r@gmail.com>
2022-11-01 00:40:56 +0530

    Completed sign in screen

--- iOS/Presentation/Unauthenticated/Screens/SignIn/SignInView.swift
+++ iOS/Presentation/Unauthenticated/Screens/SignIn/SignInView.swift
@@ -11,23 +11,88 @@ import AuthenticationServices
 
 struct SignInView: View {
     @EnvironmentObject var authViewModel: AuthenticationViewModel
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        VStack {
-            GoogleSignInButton(scheme: .light, style: .wide, state: .normal) {
-                authViewModel.signInWithGoogle()
-            }
+        Image("Text Logo")
+        Text("Independent Fundamental Baptist preaching from around the world")
+            .multilineTextAlignment(.center)
+            .italic()
             .padding()
-            
-            SignInWithAppleButton(.signIn) { aSAuthorizationAppleIdRequest in
-               authViewModel.configure(appleSignInAuthorizationRequest: aSAuthorizationAppleIdRequest)
-            } onCompletion: { result in
-                authViewModel.signInWithApple(requestAuthorizationResult: result)
+        
+        Spacer()
+        
+        Text("Sign in to get access to free content and get personalized recommendations")
+            .padding()
+        
+        VStack {
+            if colorScheme == .light {
+                Button {
+                    authViewModel.signInWithGoogle()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Image("Google Logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                        Text("Sign in with Google")
+                            .font(.custom("Gill Sans", size: 20))
+                            .foregroundColor(.blue)
+                        Spacer()
+                    }
+                    .frame(height: 45)
+                    .background(.white)
+                    .cornerRadius(7)
+                    .buttonBorderShape(.roundedRectangle(radius: 7))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 7)
+                            .stroke(.gray, lineWidth: 1)
+                    )
+                    .padding()
+                }
+                
+                SignInWithAppleButton(.signIn) { aSAuthorizationAppleIdRequest in
+                   authViewModel.configure(appleSignInAuthorizationRequest: aSAuthorizationAppleIdRequest)
+                } onCompletion: { result in
+                    authViewModel.signInWithApple(requestAuthorizationResult: result)
+                }
+                .signInWithAppleButtonStyle(.black)
+                .frame(height: 45)
+                .padding(.horizontal)
+                .padding(.bottom)
+            } else {
+                Button {
+                    authViewModel.signInWithGoogle()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Image("Google Logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                        Text("Sign in with Google")
+                            .font(.custom("Gill Sans", size: 20))
+                            .foregroundColor(.blue)
+                        Spacer()
+                    }
+                    .frame(height: 45)
+                    .background(.white)
+                    .cornerRadius(7)
+                    .buttonBorderShape(.roundedRectangle(radius: 7))
+                    .padding()
+                }
+                
+                SignInWithAppleButton(.signIn) { aSAuthorizationAppleIdRequest in
+                   authViewModel.configure(appleSignInAuthorizationRequest: aSAuthorizationAppleIdRequest)
+                } onCompletion: { result in
+                    authViewModel.signInWithApple(requestAuthorizationResult: result)
+                }
+                .signInWithAppleButtonStyle(.white)
+                .frame(height: 45)
+                .padding(.horizontal)
+                .padding(.bottom)
             }
-            .frame(height: 45)
-            .padding(.horizontal)
-
-          Spacer()
         }
     }
 }
