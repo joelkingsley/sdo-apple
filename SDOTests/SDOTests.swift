80504bdecbe002292c4691c4dcbafff890443f1f
Joel Kingsley <joelkingsley.r@gmail.com>
2025-06-08 17:51:06 +0200

    Created SDOTests target and moved all tests there

---
+++ SDOTests/SDOTests.swift
@@ -0,0 +1,16 @@
+//
+//  SDOTests.swift
+//  SDOTests
+//
+//  Created by Joel Kingsley Rangarajan on 08.06.25.
+//
+
+import Testing
+
+struct SDOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
